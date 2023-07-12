dbb7f0d3c877a31bbc38f01c1e63be0f2d601013
Alvis <alvis.vonogs@gmail.com>
2023-07-12 19:49:49 +0300

    v4

---
+++ scripts/exercises.js
@@ -0,0 +1,59 @@
+const person = { firstname: "John", lastname: "Doe" };
+person.age = 50;
+console.log(person.age);
+
+let vards = "Alvis";
+let vecums = 28;
+let irStudents = false;
+console.log(vards, vecums, irStudents);
+
+const augli = ["abols", "banans", "persiks"];
+console.log(augli);
+
+const persona = { name: "Alvis"};
+persona.ageis = 50;
+persona.irStudentsis = false
+console.log(persona);
+
+function sayGreeting(firstName, day, temperature){
+    console.log(`Hello, ${firstName}! Today is ${day},${temperature} degree outside!`);
+};
+sayGreeting("Alvis","Wednesday","33");
+
+//4.uzdevums
+
+function myFunction(){
+    let x = 5;
+    console.log(x);
+}
+
+myFunction();
+
+//5.uzdevums
+
+let y = 10;
+function myFunction2 (){
+
+    console.log(y)
+}
+
+myFunction2();
+
+//6.uzdevums
+
+function myFunction3(){
+    let z = 7;
+    console.log (z);
+}
+
+myFunction3();
+
+//7.uzdevums
+
+let u = 10;
+function myFunction4 (){
+u = 11;
+    console.log(u);
+}
+
+myFunction4();
